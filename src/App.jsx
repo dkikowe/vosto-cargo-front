c8cd059546fc7ae59457cfc53d4d16b112a802e1
dkikowe <dkikowe@gmail.com>
2025-03-15 05:09:29 +0500

    fx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Nav from "./components/NavMenu/Nav";
 import Cabinet from "./Pages/Cabinet/Cabinet";
 
 function App() {
+  useEffect(() => {
+    const tg = window.Telegram.WebApp;
+    // tg.requestFullscreen();
+    tg.ready();
+
+    return () => {
+      tg.close(); // Закрытие веб-приложения (при необходимости)
+    };
+  }, []);
   return (
     <Router>
       <Header />
