f1bfec5535041a4c2670243e759cea87602fcf24
dkikowe <dkikowe@gmail.com>
2025-03-23 04:42:32 +0500

    done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,5 @@
 import s from "./Header.module.sass";
 
 export default function Header() {
-  return (
-    <div className={s.header}>
-      <img src="/images/header-icons/logo.svg" alt="" className={s.logo} />
-    </div>
-  );
+  return <div className={s.header}></div>;
 }
