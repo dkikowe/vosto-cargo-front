b7d674851a66a2bd0e8dc1a947de1810ed4f1142
dkikowe <dkikowe@gmail.com>
2025-03-29 05:59:02 +0500

    done home

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://vosto-cargo-back-production.up.railway.app/",
-  // baseURL: "http://localhost:5050",
+  // baseURL: "https://vosto-cargo-back-production.up.railway.app",
+  baseURL: "http://localhost:5050",
 });
 
 instance.interceptors.request.use((config) => {
