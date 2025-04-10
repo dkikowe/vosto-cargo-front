fd60d5a4ad4b2efa3bd7e9d247ae032a20b05971
dkikowe <dkikowe@gmail.com>
2025-04-10 14:44:29 +0500

    s

--- src/Pages/Start/Start.jsx
+++ src/Pages/Start/Start.jsx
@@ -1,15 +1,14 @@
+import { useEffect } from "react";
 import s from "./Start.module.sass";
 
 export default function Start() {
+  useEffect(() => {
+    Telegram.WebApp.openLink("https://vostokargo.pro/");
+  }, []);
+
   return (
-    <div className={s.iframeContainer}>
-      <iframe
-        src="https://vostokargo.pro/" // заменяешь на сайт заказчика
-        width="100%"
-        height="100%"
-        frameBorder="0"
-        allowFullScreen
-      />
+    <div className={s.linkContainer}>
+      <p>Перенаправление на сайт заказчика...</p>
     </div>
   );
 }
