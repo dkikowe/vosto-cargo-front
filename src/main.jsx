4ea769f52f9552326954f510bfd6e10f757437a6
dkikowe <dkikowe@gmail.com>
2025-04-23 20:23:39 +0500

    sds

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,6 @@ import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
 import "./i18n";
 
-window.onload = () => {
-  setTimeout(() => {
-    if (!window.location.hash || window.location.hash === "#/") {
-      window.location.replace(window.location.origin + "/#/home");
-    }
-  }, 100);
-};
-
 ReactDOM.render(
   <ThemeProvider>
     <App />
